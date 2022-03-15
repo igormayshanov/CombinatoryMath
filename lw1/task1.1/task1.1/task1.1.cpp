c5fe420af499020d28276cdc59744812bc6c4ccf
igormayshanov <79751645+igormayshanov@users.noreply.github.com>
2022-03-16 02:54:07 +0500

    add task 1.1

---
+++ lw1/task1.1/task1.1/task1.1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <boost/timer.hpp>
+
+int main()
+{
+    std::vector<size_t> v = { 7, 1, 12, 5, 9, 20, 15 };
+    boost::timer t;
+    t.restart();
+    std::sort(v.begin(), v.end());
+    do
+    {
+        copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+        std::cout << "\n";
+    } while (std::next_permutation(v.begin(), v.end()));
+    double duration = t.elapsed();
+	std::cout << duration << "\n";
+}
+
