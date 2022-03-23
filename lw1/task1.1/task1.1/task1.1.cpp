e187e837301ca5038d5f2e9c8f6ef16a45fe8515
igormayshanov <79751645+igormayshanov@users.noreply.github.com>
2022-03-23 09:59:18 +0500

    add result of task1.1

--- lw1/task1.1/task1.1/task1.1.cpp
+++ lw1/task1.1/task1.1/task1.1.cpp
@@ -6,16 +6,19 @@
 
 int main()
 {
-    std::vector<size_t> v = { 7, 1, 12, 5, 9, 20, 15 };
+	std::vector<size_t> currentPermutation = { 7, 1, 12, 5, 9, 20, 15, 2, 3, 4, 8, 6, 10, 11 };
+	std::vector<size_t> reversePermutation = {};
+
+
+	//, 6, 10, 11, 13
     boost::timer t;
     t.restart();
     std::sort(v.begin(), v.end());
     do
     {
-        copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
-        std::cout << "\n";
+       // copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+     //   std::cout << "\n";
     } while (std::next_permutation(v.begin(), v.end()));
     double duration = t.elapsed();
 	std::cout << duration << "\n";
 }
-
