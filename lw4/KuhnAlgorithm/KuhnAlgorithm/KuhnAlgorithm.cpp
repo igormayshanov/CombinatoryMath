211fb5a2bcd1aee34ad61ff53a53840b976c2258
igormayshanov <79751645+igormayshanov@users.noreply.github.com>
2022-05-23 16:31:34 +0500

    add KuhnAlgorithm

--- lw4/KuhnAlgorithm/KuhnAlgorithm/KuhnAlgorithm.cpp
+++ lw4/KuhnAlgorithm/KuhnAlgorithm/KuhnAlgorithm.cpp
@@ -1,19 +1,6 @@
-﻿#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <optional>
-#include <sstream>
-#include <string>
-#include <vector>
-
-#include "functions.h"
+﻿#include "functions.h"
 
 using namespace std;
-
-std::vector<std::vector<int>> GetZeroEdges(const std::vector<std::vector<int>>& matrix);
-bool DFS(int v, std::vector<std::vector<int>> zeroEdges,
-	std::vector<char>& used, std::vector<int>& matchings);
-
 
 int main(int argc, char* argv[])
 {
@@ -37,23 +24,20 @@ int main(int argc, char* argv[])
 
 	cout << "Print input matrix\n";
 	PrintMatrix(inputMatrix);
-	auto matrix = TransformMatrix(inputMatrix);
-	cout << "Print transform matrix\n";
-	PrintMatrix(matrix);
 
-	auto zeroEdges = GetZeroEdges(matrix);
+	auto edges = GetEdges(inputMatrix);
 
-	PrintMatrix(zeroEdges);
+	PrintMatrix(edges);
 
-	std::vector<int> matchings(matrix.size(), -1);
+	std::vector<int> matchings(p, -1);
 	std::vector<char> used;
 
-	for (int v = 0; v < zeroEdges.size(); ++v) {
-		used.assign(zeroEdges.size(), false);
-		DFS(v, zeroEdges, used, matchings);
+	for (int v = 0; v < edges.size(); ++v) {
+		used.assign(edges.size(), false);
+		DFS(v, edges, used, matchings);
 	}
 	bool isMaxMatching = true;
-	
+
 	for (int i = 0; i < matchings.size(); ++i)
 	{
 		if (matchings[i] == -1)
@@ -62,14 +46,8 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	if (isMaxMatching)
-	{
-		cout << "Yes\n";
-	}
-	else
-	{
-		cout << "No\n";
-		PrintMatchings(matchings);
-	}
+	isMaxMatching ? cout << "Yes\n" : cout << "No\n";
+	PrintMatchings(matchings);
+
 	return 0;
 }
--- lw4/KuhnAlgorithm/KuhnAlgorithm/functions.cpp
+++ lw4/KuhnAlgorithm/KuhnAlgorithm/functions.cpp
@@ -29,14 +29,12 @@ std::optional<std::ifstream> OpenFile(std::string fileName)
 
 std::vector<std::vector<int>> GetAdjacencyMatrix(std::ifstream& inputFile, int p, int q)
 {
-	int tempNum;
 	std::vector<std::vector<int>> adjacencyMatrix(p, vector<int>(q, 0));
 	for (int i = 0; i < p; i++)
 	{
 		for (int j = 0; j < q; j++)
 		{
-			inputFile >> tempNum;
-			tempNum != 0 ? adjacencyMatrix[i][j] = tempNum : adjacencyMatrix[i][j] = INT_MAX;
+			inputFile >> adjacencyMatrix[i][j];
 		}
 	}
 	return adjacencyMatrix;
@@ -72,32 +70,32 @@ std::vector<std::vector<int>> TransformMatrix(std::vector<std::vector<int>>& inp
 	return matrix;
 }
 
-std::vector<std::vector<int>> GetZeroEdges(const std::vector<std::vector<int>>& matrix)
+std::vector<std::vector<int>> GetEdges(const std::vector<std::vector<int>>& matrix)
 {
-	std::vector<std::vector<int>> zeroEdges(matrix.size());
+	std::vector<std::vector<int>> edges(matrix.size());
 	for (int i = 0; i < matrix.size(); i++)
 	{
-		for (int j = 0; j < matrix.size(); j++)
+		for (int j = 0; j < matrix[i].size(); j++)
 		{
-			if (matrix[i][j] == 0)
+			if (matrix[i][j] == 1)
 			{
-				zeroEdges[i].push_back(j);
+				edges[i].push_back(j);
 			}
 		}
 	}
-	return zeroEdges;
+	return edges;
 }
 
-bool DFS(int v, std::vector<std::vector<int>> zeroEdges,
+bool DFS(int v, std::vector<std::vector<int>> edges,
 	std::vector<char>& used, std::vector<int>& matchings)
 {
 	if (used[v])
 		return false;
 	used[v] = true;
-	for (int i = 0; i < zeroEdges[v].size(); ++i)
+	for (int i = 0; i < edges[v].size(); ++i)
 	{
-		int to = zeroEdges[v][i];
-		if (matchings[to] == -1 || DFS(matchings[to], zeroEdges, used, matchings))
+		int to = edges[v][i];
+		if (matchings[to] == -1 || DFS(matchings[to], edges, used, matchings))
 		{
 			matchings[to] = v;
 			return true;
@@ -105,7 +103,6 @@ bool DFS(int v, std::vector<std::vector<int>> zeroEdges,
 	}
 	return false;
 }
-
 
 void PrintMatrix(const vector<vector<int>>& matrix)
 {
@@ -124,28 +121,5 @@ void PrintMatchings(const std::vector<int> vector)
 	for (int i = 0; i < vector.size(); ++i)
 	{
 		std::cout << vector[i] + 1 << " - " << i + 1 << "\n";
-	}
-}
-
-
-int GetMinElemAboveZeroInVector(const std::vector<int>& vector)
-{
-	int min = INT_MAX;
-	for (auto& i : vector)
-	{
-		if (i != 0)
-			min = min < i ? min : i;
-	}
-	return min;
-}
-
-void SubtractionMinElem(std::vector<int>& vector, int min)
-{
-	for (auto& i : vector)
-	{
-		if (i != 0)
-		{
-			i == INT_MAX ? i = INT_MAX : i -= min;
-		}
 	}
 }
