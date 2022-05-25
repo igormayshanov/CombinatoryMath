a42834adc11c6fc8bcdf1a3c012f9da2c72656c0
igormayshanov <79751645+igormayshanov@users.noreply.github.com>
2022-05-25 23:16:56 +0300

    add lw1

---
+++ lw1/lw1/task1.5/task1.5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <boost/timer.hpp>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Enter number of elements: ";
+    cin >> N;
+    cout << "\n";
+
+    int k;
+    cout << "Enter number of placements: ";
+    cin >> k;
+    cout << "\n";
+
+    boost::timer t;
+    t.restart();
+
+    vector<int> v(k + 1, 0);
+
+    while (v[k] != 1)
+    {
+        copy(v.begin(), v.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
+        cout << "\n";
+        int i = 0;
+        while (v[i] == N - 1)
+        {
+            v[i] = 0;
+            ++i;
+        }
+        ++v[i];
+    }
+    double duration = t.elapsed();
+    cout << duration << "\n";
+}
---
+++ lw1/lw1/task1.6/task1.6.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <boost/timer.hpp>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cout << "Enter number of elements: ";
+	cin >> N;
+	cout << "\n";
+
+	boost::timer t;
+	t.restart();
+
+	vector<int> v(N, 0);
+
+	while (v[v.size() - 1] != 1)
+	{
+		copy(v.begin(), v.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
+		cout << "\n";
+		int i = 0;
+		while (v[i] == 1)
+		{
+			v[i] = 0;
+			i = i + 1;
+		}
+		v[i] = 1;
+	}
+
+	double duration = t.elapsed();
+	cout << duration << "\n";
+}
